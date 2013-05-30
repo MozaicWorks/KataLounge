d6fc69ac1345c2fc89fd2411165abd210267ca5c
Catalin <catalin.lazar@orange.com>
2013-05-30 15:26:58 +0300

    poker: added behavior for 1 card game

--- lazcatluc/poker/src/ro/lazcatluc/kata/poker/Poker.java
+++ lazcatluc/poker/src/ro/lazcatluc/kata/poker/Poker.java
@@ -9,7 +9,19 @@ package ro.lazcatluc.kata.poker;
  * @author Catalin
  */
 public class Poker {
+    private final Player player2;
+    private final Player player1;
+    
+    public Poker(Player player1, Player player2) {
+        this.player1 = player1;
+        this.player2 = player2;
+    }
+    
     public boolean isDraw() {
         return true;
+    }
+    
+    public Player getWinner() {
+        return player1;
     }
 }
--- lazcatluc/poker/test/ro/lazcatluc/kata/poker/PokerTest.java
+++ lazcatluc/poker/test/ro/lazcatluc/kata/poker/PokerTest.java
@@ -13,10 +13,29 @@ import static org.junit.Assert.*;
  */
 public class PokerTest { 
 
-
     @Test
     public void noCardsGameIsDraw() {
-        Poker poker = new Poker();
+        Poker poker = new Poker(new Player(0), new Player(0));
+        
+        assertTrue(poker.isDraw());
+    }
+    
+    @Test
+    public void oneCardFirstCardHigherFirstPlayerWins() throws Exception {
+        Player player1 = new Player(1);
+        Player player2 = new Player(0);
+        
+        Poker poker = new Poker(player1, player2);
+        
+        assertEquals(player1, poker.getWinner());
+    }
+    
+    @Test
+    public void oneCardEqualCardsIsDraw() throws Exception {
+        Player player1 = new Player(1);
+        Player player2 = new Player(1);
+        
+        Poker poker = new Poker(player1, player2);
         
         assertTrue(poker.isDraw());
     }
