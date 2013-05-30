d7964bf71c9eed5df08f00e87b80fbe1bbdcc493
unknown <eduardcs@HI36231.cegekanv.corp.local>
2013-05-30 14:37:34 +0300

    start args - Eduard Stanculet

---
+++ Stanculet/Args/Kata.Args.Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kata.Args.Test
+{
+    [TestClass]
+    public class TestParse
+    {
+        [TestMethod]
+        public void Test()
+        {
+        }
+    }
+}
