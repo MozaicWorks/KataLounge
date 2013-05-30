5f4eee5e28c911e7e2702cefffc6b464c542702b
Catalin <catalin.lazar@orange.com>
2013-05-30 15:13:12 +0300

    poker initial commit

---
+++ lazcatluc/poker/src/ro/lazcatluc/kata/poker/Poker.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ro.lazcatluc.kata.poker;
+
+/**
+ *
+ * @author Catalin
+ */
+public class Poker {
+
+}
