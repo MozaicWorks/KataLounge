6bd4177eb5409e46bd722e360f6113ccb35950c0
Catalin <catalin.lazar@orange.com>
2013-05-30 11:49:20 +0300

    bankocr: added behavior for reading 2

---
+++ lazcatluc/bankocr/test/ro/lazcatluc/kata/bankocr/ReadOneNumberTest.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ro.lazcatluc.kata.bankocr;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Catalin
+ */
+public class ReadOneNumberTest {
+    
+    public ReadOneNumberTest() {
+    }
+    
+    @Test
+    public void readOneIs1() throws Exception {
+        String one = 
+        "   "+
+        "  |"+
+        "  |";
+        
+        assertEquals(1, parse(one));
+    }
+    
+    @Test
+    public void readTwoIs2() throws Exception {
+        String two = 
+        " _ "+
+        " _|"+
+        "|_ ";
+        
+        assertEquals(2, parse(two));
+    }
+    
+    int parse(String s) {
+        return s.contains("_") ? 2 : 1;
+    }
+}
