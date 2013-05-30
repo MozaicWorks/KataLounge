472e43ce3e482ce61a63f1e79014d8e76996798e
Catalin <catalin.lazar@orange.com>
2013-05-30 16:57:03 +0300

    added behavior for roll 0, 0

---
+++ lazcatluc/bowling/src/ro/lazcatluc/bowling/Bowling.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ro.lazcatluc.bowling;
+
+/**
+ *
+ * @author Catalin
+ */
+public class Bowling {
+
+    public void roll(int popice) {
+
+    }
+    
+    public int getResult() {
+        return 0;
+    }
+}
---
+++ lazcatluc/bowling/test/ro/lazcatluc/bowling/BowlingTest.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ro.lazcatluc.bowling;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Catalin
+ */
+public class BowlingTest {
+    
+
+    @Test
+    public void nothingGoesToZero() {
+        Bowling bowling = new Bowling();
+        int expectedResult = 0;
+        
+        bowling.roll(0);
+        bowling.roll(0);
+        int result = bowling.getResult();
+        
+        assertEquals(expectedResult, result);
+    }
+}
