825b7d2d9ee46b210f2071dec5b1f6f57a379f3f
Catalin <catalin.lazar@orange.com>
2013-05-30 12:46:55 +0300

    added behavior for empty board

--- lazcatluc/reversi/test/ro/lazcatluc/kata/reversi/ReversiTest.java
+++ lazcatluc/reversi/test/ro/lazcatluc/kata/reversi/ReversiTest.java
@@ -4,10 +4,6 @@
  */
 package ro.lazcatluc.kata.reversi;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -17,28 +13,20 @@ import static org.junit.Assert.*;
  */
 public class ReversiTest {
     
-    public ReversiTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
 
     @Test
-    public void testSomeMethod() {
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void finishOnEmptyBoard() {
+        emptyBoard();
+        
+        assertTrue(isFinished());
     }
+    
+    boolean isFinished() {
+        return true;
+    }
+    
+    void emptyBoard() {
+        
+    }
+   
 }
