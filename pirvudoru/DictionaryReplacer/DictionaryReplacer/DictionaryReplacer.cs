43be06149a76f62b5174b5ddcc255df4edc959ab
Pirvu Doru Lucian <doru.pirvu@agilefreaks.com>
2013-05-31 15:39:30 +0300

    extract constant

--- pirvudoru/DictionaryReplacer/DictionaryReplacer/DictionaryReplacer.cs
+++ pirvudoru/DictionaryReplacer/DictionaryReplacer/DictionaryReplacer.cs
@@ -5,9 +5,11 @@ namespace DictionaryReplacer
 {
     public class DictionaryReplacer
     {
+        private const string WildCard = "$";
+
         public void ReplacePlaceholderKeys(Dictionary<string, string> target)
         {
-            var keys = target.Keys.Where(k => k.StartsWith("$") && k.EndsWith("$")).ToList();
+            var keys = target.Keys.Where(k => k.StartsWith(WildCard) && k.EndsWith(WildCard)).ToList();
 
             foreach (var key in keys)
             {
