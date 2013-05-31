747ef822285c16b19a7b7fd43744d92eaf0f1d97
Catalin <catalin.lazar@orange.com>
2013-05-31 11:10:43 +0300

    refactored X and turn out of test method

--- lazcatluc/tictactoe/test/tictactoe/TictactoeTest.java
+++ lazcatluc/tictactoe/test/tictactoe/TictactoeTest.java
@@ -6,19 +6,34 @@ package tictactoe;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Ignore;
 
 /**
  *
  * @author Catalin
  */
 public class TictactoeTest {
+    private Object X = new Object();
+    private Object turn = X;
     
    
     @Test
     public void xPlaysFirst() {
-        Object X = new Object();
-        Object turn = X;
         
         assertEquals(X, turn);
+    }
+    
+    @Test
+    @Ignore
+    public void zPlaysSecond() throws Exception {
+        Object Z = new Object();
+        
+        play();
+        
+        assertEquals(Z, turn);
+    }
+
+    private void play() {
+        
     }
 }
