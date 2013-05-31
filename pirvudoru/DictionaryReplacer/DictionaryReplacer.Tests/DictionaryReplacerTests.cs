17c39e999dccf7c5d5a609e6d1673c8eee2cf048
Pirvu Doru Lucian <doru.pirvu@agilefreaks.com>
2013-05-31 15:31:57 +0300

    replacer does not replace keys that only start with $

--- pirvudoru/DictionaryReplacer/DictionaryReplacer.Tests/DictionaryReplacerTests.cs
+++ pirvudoru/DictionaryReplacer/DictionaryReplacer.Tests/DictionaryReplacerTests.cs
@@ -23,5 +23,15 @@ namespace DictionaryReplacer.Tests
 
             Assert.IsTrue(dictionary.ContainsKey("key"));
         }
+
+        [Test]
+        public void Replace_Always_DoestNotReplaceKeysThatOnlyHaveALeadingDollarSign()
+        {
+            var dictionary = new Dictionary<string, string> { { "$key", "value" } };
+
+            _subject.ReplacePlaceholderKeys(dictionary);
+
+            Assert.IsTrue(dictionary.ContainsKey("$key"));
+        }
     }
 }
