8a91272059240ef692fb689102592527b66dd7d7
Catalin <catalin.lazar@orange.com>
2013-05-31 11:03:30 +0300

    tictactoe: initial commit

---
+++ lazcatluc/tictactoe/test/tictactoe/TictactoeTest.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tictactoe;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Catalin
+ */
+public class TictactoeTest {
+    
+   
+    @Test
+    public void test() {
+        
+    }
+}
